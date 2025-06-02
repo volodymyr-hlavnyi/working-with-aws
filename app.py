574ca5503e2371e5a347c1d6a5e2fcc0a5b8eee3
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2025-06-02 17:14:14 +0200

    ver_04_add_error_proccess

--- app.py
+++ app.py
@@ -22,41 +22,65 @@ def get_credentials():
 
 def get_session():
     credentials = get_credentials()
-    session = boto3.Session(
-        aws_access_key_id=credentials['aws_access_key_id'],
-        aws_secret_access_key=credentials['aws_secret_access_key'],
-        region_name=env.get("AWS_REGION", "us-east-1")
-    )
+    session = None
+    try:
+        session = boto3.Session(
+            aws_access_key_id=credentials['aws_access_key_id'],
+            aws_secret_access_key=credentials['aws_secret_access_key'],
+            region_name=env.get("AWS_REGION", "us-east-1")
+        )
+    except Exception as e:
+        print(f"Error creating AWS session: {e}")
+
     return session
 
 
 def get_ec2_instances():
     session = get_session()
-    ec2 = session.client('ec2')
-    response = ec2.describe_instances()
-    return response
+    if not session:
+        ec2 = session.client('ec2')
+        response = ec2.describe_instances()
+        return response
+    print("Failed to create AWS session.")
+    return None
 
 
 def get_s3_buckets():
     session = get_session()
-    s3 = session.client('s3')
-    response = s3.list_buckets()
-    return response
+    if not session:
+        s3 = session.client('s3')
+        response = s3.list_buckets()
+        return response
+    print("Failed to create AWS session.")
+    return None
 
 
 def get_users():
     session = get_session()
-    iam = session.client('iam')
-    response = iam.list_users()
-    return response
+    if not session:
+        iam = session.client('iam')
+        response = iam.list_users()
+        return response
+    print("Failed to create AWS session.")
+    return None
 
 
 if __name__ == "__main__":
     instances = get_ec2_instances()
-    print(f"EC2 Instances: {instances}")
+    if instances:
+        print(f"EC2 Instances: {instances}")
+    else:
+        print("No EC2 instances found or error retrieving instances.")
+
 
     buckets = get_s3_buckets()
-    print(f"S3 Buckets: {buckets['Buckets'] if 'Buckets' in buckets else 'No buckets found'}")
+    if buckets:
+        print(f"S3 Buckets: {buckets['Buckets'] if 'Buckets' in buckets else 'No buckets found'}")
+    else:
+        print("No S3 buckets found or error retrieving buckets.")
 
     users = get_users()
-    print(f"IAM Users: {users['Users'] if 'Users' in users else 'No users found'}")
+    if users:
+        print(f"IAM Users: {users['Users'] if 'Users' in users else 'No users found'}")
+    else:
+        print("No IAM users found or error retrieving users.")
